2f3c9880ae89ad8ebdb9bc5c80ddb6069d724544
Drantho <drantho@gmail.com>
2021-03-04 08:04:08 -0800

    deploy

--- server.js
+++ server.js
@@ -6,15 +6,6 @@ require('dotenv').config();
 const db = require("./models");
 const app = express();
 
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-
-const { ExpressPeerServer } = require('peer');
-const peerServer = ExpressPeerServer(server, {
-    debug: true
-});
-
-app.use('/peerjs', peerServer);
 app.use(compression());
 app.use(cors())
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
@@ -24,26 +15,13 @@ const routes = require("./controllers/routes.js");
 
 app.use(routes);
 
-if (process.env.NODE_ENV === "production") {
+if(process.env.NODE_ENV === "production"){
     app.use(express.static("../client/build"))
 }
 
-io.on('connection', socket => {
-    console.log(`socket.on()connection`);
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId)
-        socket.to(roomId).broadcast.emit('user-connected', userId);
-        console.log(`socket.on(join-room) fires`);
-
-        socket.on('disconnect', () => {
-            socket.to(roomId).broadcast.emit('user-disconnected', userId)
-        })
-    })
-})
-
 const PORT = process.env.PORT || 3001;
-db.sequelize.sync({ force: false }).then(function () {
-    server.listen(PORT, function () {
+db.sequelize.sync({ force: false}).then(function () {
+    app.listen(PORT, function () {
         console.log(`App now listening on port: ${PORT} view at: http://localhost:${PORT}`);
     });
 });
